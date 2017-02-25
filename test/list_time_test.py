1ea8f9e263ddbc60d0d7cee69c1fb9abebd79587
Oscar <oscarbc1996@gmail.com>
2017-02-26 00:46:41 +0100

    tested remove unannounced

--- test/list_time_test.py
+++ test/list_time_test.py
@@ -10,6 +10,14 @@ class ListTimeTest(unittest.TestCase):
         result = list.get_peers()
         self.assertEqual([1, 2, 3], result)
 
+    def test_remove_unannounced(self):
+        list = ListTime(1)
+        list.update(2)
+        list.update(3)
+        list.remove_unannounced(diffTime=0)
+        result = list.get_peers()
+        self.assertEqual([], result)
+
 
 if __name__ == '__main__':
     unittest.main()
