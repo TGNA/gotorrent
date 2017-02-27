cc94f31f88f1469e79cee3813ce1b04f7b7c5e82
VictorColome <victorcolomec@gmail.com>
2017-02-27 12:47:01 +0100

    peer_test

---
+++ test/peer_test.py
@@ -0,0 +1,32 @@
+import unittest
+from gotorrent.peer import *
+from gotorrent.tracker import *
+from pyactor.context import set_context, create_host, sleep
+
+class PeerTest(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        set_context()
+        self.host = create_host()
+        # Spawn tracker and peers
+        self.tracker = self.host.spawn('tracker', Tracker)
+        self.p1 = self.host.spawn('peer1', Peer)
+        self.p2 = self.host.spawn('peer2', Peer)
+        self.p3 = self.host.spawn('peer3', Peer)
+        # Attach tracker to peers
+        self.p1.attach_tracker(self.tracker)
+        self.p2.attach_tracker(self.tracker)
+        self.p3.attach_tracker(self.tracker)
+
+    def test_peer(self):
+        self.tracker.init_start()
+        self.p1.init_start()
+        self.p2.init_start()
+        self.p3.init_start()
+        sleep(1)
+        self.assertEqual(set(['peer1', 'peer2', 'peer3']), set(self.tracker.get_peers("file")))
+        sleep(30)
+        self.assertEqual([], self.tracker.get_peers("file"))
+
+if __name__ == '__main__':
+    unittest.main()
