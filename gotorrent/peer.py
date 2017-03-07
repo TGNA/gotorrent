f52b6a031da0a27087137dd01434ca23f3604a23
Oscar <oscarbc1996@gmail.com>
2017-03-07 20:37:30 +0100

    push method

--- gotorrent/peer.py
+++ gotorrent/peer.py
@@ -3,6 +3,9 @@ class Peer(object):
     _ask = ['get_id']
     _ref = ['attach_tracker']
 
+    def __init__(self):
+        self.data = {}
+
     def init_start(self):
         self.interval = self.host.interval(3, self.proxy, 'announce_me')
         self.host.later(5, self.proxy, 'stop_interval')
@@ -18,3 +21,7 @@ class Peer(object):
 
     def attach_tracker(self, tracker):
         self.tracker = tracker
+
+    def push(self, chunk_id, chunk_data):
+        self.data[chunk_id] = chunk_data
+
